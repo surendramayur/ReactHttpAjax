c88d4788fb1d6ddfcc2cf7964c4e403ab9b05e2e
Mayur <surendramayur@gmail.com>
2018-10-20 13:18:29 +0530

    making post selectable and fetching data on update without creatimg loop

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
     
     componentDidMount() {
@@ -27,11 +28,16 @@ class Blog extends Component {
             });
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
     render () {
 
         const posts = this.state.posts.map(post => {
             return <Post 
                 key={post.id} 
+                clicked={() => this.postSelectedHandler(post.id)}
                 author={post.author}
                 title={post.title} />;
         });
@@ -42,7 +48,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
